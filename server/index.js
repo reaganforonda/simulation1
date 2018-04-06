98fcfcebf2b3046ac3492785c641b39e9969d0bc
reaganforonda <foronda.reagan@gmail.com>
2018-04-06 10:51:32 -0600

    completed Server

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const controller = require('./controller.js');
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const port = 3010;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(corse());
+app.use(cors());
 
 
 
-app.listen(porst, ()=> {
+app.listen(port, ()=> {
     console.log(`Creepin' on Port: ${port}`)
 })
